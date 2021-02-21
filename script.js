8306580540f40ff0ac5ea89281dfd95e22d3a4c8
Cynthia Yeung <cynthia_yeung@apple.com>
2021-02-21 01:11:13 -0600

    Component, Dropdown: Add JS mouse events for click and hover, add safari polyfill for smooth scrolling, remove CSS hover styles and update naming conventions for readability/clarity

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+	// Dropdown Mouse Events
+	const dropdownButton = document.querySelector('.dropdown-button');
+	const dropdownOptions = document.querySelector('.dropdown-options');
+	const dropdownSelect = document.querySelector('.dropdown-select');
+	
+	function clickDropdown() {
+		const hasShowClass = dropdownButton.classList.contains('dropdown-options--expanded');
+		dropdownButton.classList.add('dropdown-options--expanded');
+		dropdownButton.setAttribute('aria-expanded', hasShowClass ? 'false' : 'true');
+		dropdownOptions.scrollIntoView({behavior: "smooth", block: "start"});
+		
+		if (hasShowClass) { 
+			const sectionTop = document.querySelector('.section-image');
+			sectionTop.scrollIntoView({behavior: "smooth", block: "start"});
+			dropdownButton.classList.remove('dropdown-options--expanded');
+		}
+	}
+
+	function expandDropdown() {
+		dropdownButton.classList.add('dropdown-options--expanded');
+		dropdownButton.setAttribute('aria-expanded', 'true');
+		dropdownOptions.scrollIntoView({behavior: "smooth", block: "start"});
+	}
+
+  function collapseDropdown() {
+    dropdownButton.classList.remove('dropdown-options--expanded');
+    dropdownButton.setAttribute('aria-expanded', 'false');
+
+		// Bespoke feature to scroll up when dropdown closes
+		const sectionTop = document.querySelector('.section-image');
+		sectionTop.scrollIntoView({behavior: "smooth", block: "start"});
+  }
+
+	dropdownButton.addEventListener('click', clickDropdown);
+	dropdownButton.addEventListener('mouseover', expandDropdown);
+	dropdownSelect.addEventListener('mouseleave', collapseDropdown);
+});
+
